c5e71096cb549ce9d3a623a23e9879547f4d5849
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-25 19:11:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,6 +13,16 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
-  # Code your final two routes here:
+  # Code your final two routes here: 
+  
+  get "/goodbye/:name" do 
+    "Goodbye, #{params[:name]}."
+  end
+  get "/multiply/:num1/:num2" do 
+    # "#{params[:num1] * params[:num2]}"
+    
+    "<!DOCTYPE html>\n<html>\n<head>\n  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=ut...nabled the <code>show_exceptions</code>setting.</p>\n  </div> <!-- /WRAP -->\n #{params[:num1] * params[:num2]} </body>\n</html>\n"
+    erb :multiply
+  end
 
 end
